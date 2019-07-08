b35550bdda10d4d78a095ba7bc16664d3a5508a9
Arsenii <arseniysmart@gmail.com>
2019-07-08 15:39:34 +0300

    Created Typer-Element component

---
+++ src/components/Typer-Element.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>{{ text }}<span>&nbsp;</span></p>
+</template>
+
+<script>
+  export default {
+    name: "Typer-Element",
+    props: {
+      text: String
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    font-size: 200%;
+  }
+
+  span {
+    border-left: 2px solid;
+    -webkit-animation: caret 1s steps(1) infinite;
+            animation: caret 1s steps(1) infinite;
+  }
+
+  @-webkit-keyframes caret {
+    50% {
+      border-color: transparent;
+    }
+  }
+
+  @keyframes caret {
+    50% {
+      border-color: transparent;
+    }
+  }
+</style>
