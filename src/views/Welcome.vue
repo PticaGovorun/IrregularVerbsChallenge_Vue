856b8811802cc3507739dece6ba8ea4911776816
Arsenii <arseniysmart@gmail.com>
2019-07-08 03:26:41 +0300

    Added Welcome-To component

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <Welcome-To id="welcome-to"/>
 </template>
 
 <script>
+  // @ is an alias to /src
+  import WelcomeTo from '@/components/Welcome-To.vue'
+
   export default {
-    name: "Welcome"
+    name: "Welcome",
+    components: {
+      WelcomeTo
+    }
   }
 </script>
 
 <style scoped>
-
+  #welcome-to {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    margin: 0 40px 0 40px;
+  }
 </style>
