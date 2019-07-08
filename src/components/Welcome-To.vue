e3ae5b59f74c5a3e48a221276a1d83132f0060c1
Arsenii <arseniysmart@gmail.com>
2019-07-08 03:23:26 +0300

    Created Welcome-To component

---
+++ src/components/Welcome-To.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Welcome to</p>
+    <h1>Irregular Verbs Challenge</h1>
+    <p>What to do? &#8594; enter three forms of verbs as many as you can.</p>
+<!--    <input id="bring-it-on_btn" type="button" value="Bring it on!" onclick=bringItOn()>-->
+<!--    <button id="bring-it-on_btn">Bring it on!</button>-->
+    <Default-Button value="Bring it on!" @click=""/>
+  </div>
+</template>
+
+<script>
+  import DefaultButton from "./Default-Button.vue";
+
+  export default {
+    name: "Welcome-To",
+    components: {
+      DefaultButton
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    margin-top: 0;
+    margin-bottom: 0;
+    font-size: 300%;
+  }
+</style>
