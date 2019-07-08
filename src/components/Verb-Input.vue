fccfbd69d18838d1da8d934b6ae15860446eaf10
Arsenii <arseniysmart@gmail.com>
2019-07-08 15:40:18 +0300

    Created Verb-Input component

---
+++ src/components/Verb-Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    type="text"
+    pattern="^[a-zA-Z\s]+$"
+    required
+    :placeholder="placeholder"
+  />
+</template>
+
+<script>
+export default {
+  name: "Verb-Input",
+  props: {
+    placeholder: String
+  }
+};
+</script>
+
+<style scoped>
+  input {
+    width: 200px;
+    text-align: center;
+    line-height: 200%;
+    font-size: 1em;
+    color: #333;
+    background: #fff;
+    border: solid 1px #d9d9d9;
+    margin-bottom: 4px;
+  }
+</style>
