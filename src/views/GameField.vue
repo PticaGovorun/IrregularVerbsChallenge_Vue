48453435373013b20ae96d3dba29fcfa96d31c19
Arsenii <arseniysmart@gmail.com>
2019-07-08 15:43:04 +0300

    Added Typer-Element and Verbs-Input-From

--- src/views/GameField.vue
+++ src/views/GameField.vue
@@ -1,13 +1,31 @@
 <template>
-  
+  <div id="container">
+    <Typer-Element text="Okay, here we go!"/>
+    <Verbs-Input-Form />
+  </div>
 </template>
 
 <script>
+  import TyperElement from "@/components/Typer-Element.vue";
+  import VerbsInputForm from "@/components/Verbs-Input-Form.vue";
+
   export default {
-    name: "GameField"
+    name: "GameField",
+    components:{
+      TyperElement,
+      VerbsInputForm
+    }
   }
 </script>
 
 <style scoped>
-
+  #container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    margin: 0 40px 0 40px;
+  }
 </style>
