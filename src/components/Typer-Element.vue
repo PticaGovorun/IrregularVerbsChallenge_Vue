19cd62c399999f3a834cd9da3174571b152c9ce1
Arsenii <arseniysmart@gmail.com>
2019-07-10 00:24:29 +0300

    Created untypeThenType() method (callback)

--- src/components/Typer-Element.vue
+++ src/components/Typer-Element.vue
@@ -11,46 +11,47 @@
     data: function () {
       return{
         text: this.propText,
+        textForTyping: "text for typing",
         typingSpeed: 100,
         untypingSpeed: 70
       }
     },
     methods: {
-      unType() {
+      untypeThenType() {
         let text = this.text;
-        let textLength = this.text.length;
+        let thisTextLength = this.text.length;
 
-        let timerId = setInterval(() => {
-          if (textLength === 0) clearInterval(timerId);
-          this.text = text.slice(0, textLength--);
+        let timerID = setInterval(() => {
+          if (thisTextLength === 0) {
+            clearInterval(timerID);
+            this.type(this.textForTyping);
+          }
+          this.text = text.slice(0, thisTextLength--);
         }, this.untypingSpeed);
-      }
-      // typingAnimation(text) {
-      //   typeWriter(0);
-      //
-      //   function typeWriter(i) {
-      //     if (i >= text.length) return;
-      //
-      //     this.text = text.substring(0, i+1) + this.span;
-      //     setTimeout(() => typeWriter(i + 1), this.typingSpeed);
-      //   }
-      // },
-      // untypingAnimation() {
-      //   untypeWriter();
-      //
-      //   function untypeWriter() {
-      //     let text1 = this.text.substring(0, this.text.length -
-      //       this.span.length - 1);
-      //
-      //     if (this.span.length < (this.text.length)) {
-      //       this.text = text1 + this.span;
-      //       setTimeout(function () {untypeWriter()}, this.untypingSpeed);
-      //     }
-      //   }
-      // }
+      },
+      untype() {
+        let text = this.text;
+        let thisTextLength = this.text.length;
+
+        let timerID = setInterval(() => {
+          if (thisTextLength === 0) clearInterval(timerID);
+          this.text = text.slice(0, thisTextLength--);
+        }, this.untypingSpeed);
+      },
+      type(text) {
+        let textLength = text.length;
+        let index = 0;
+
+        let timerID = setInterval(() => {
+          if (this.text.length === textLength) clearInterval(timerID);
+          this.text = text.slice(0, index++);
+        }, this.typingSpeed);
+      },
+
     },
     mounted: function() {
-      this.unType();
+      // setTimeout(this.type, 1500, this.textForTyping);
+      this.untypeThenType();
     }
   }
 </script>
