9795c73e4a6fd79f75ac15402f4bd229d72b23f5
Arsenii <arseniysmart@gmail.com>
2019-07-12 11:26:22 +0300

    Added methods: fetchVerbs(); data: verbs; Passing verbs through router-view as a prop

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,35 @@
 <!--      <router-link to="/welcome">Welcome</router-link> |-->
 <!--      <router-link to="/gameField">Game Field</router-link>-->
 <!--    </div>-->
-    <router-view/>
+<!--    <router-view/>-->
+    <router-view :verbs="verbs"/>
   </div>
 </template>
 
+<script>
+  export default {
+    name: "App",
+    data: function () {
+      return {
+        verbs: [["0","null","null","null","Error: could not fetch verbs"]]
+      }
+    },
+    created() {
+      this.fetchVerbs();
+    },
+    methods: {
+      fetchVerbs() {
+        this.verbs = JSON.parse(`[["1","say","said","said","говорить"],
+                                  ["2","make","made","made","делать/производить"],
+                                  ["3","go","went","gone","идти"]]`)
+      }
+    }
+  }
+</script>
+
 <style>
   @import "~normalize.css";
-  
+
   #app {
     font-family: Arial, Helvetica, sans-serif;
     color: #2e2e2e;
