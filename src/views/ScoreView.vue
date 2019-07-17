68d155a8270e84f66a000c74278369e2d39f4f70
Arsenii <arseniysmart@gmail.com>
2019-07-17 20:38:31 +0300

    Score is received and displayed

--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -1,13 +1,34 @@
 <template>
-  <h3>ScoreView</h3>
+  <div id="container">
+    <p id="score_p">{{ score }}</p>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "ScoreView"
+    name: "ScoreView",
+    props: {
+      score: {
+        type: Number,
+        required: true
+      }
+    }
   }
 </script>
 
 <style scoped>
+  #container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    margin: 0 40px 0 40px;
+  }
 
+  #score_p {
+    font-size: 250%;
+    margin: 10px;
+  }
 </style>
